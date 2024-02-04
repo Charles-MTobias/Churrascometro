efe8c8c540608417200bef06bb340843adc27f75
Charles-MTobias <micael-tobias@hotmail.com>
2024-02-04 15:01:41 -0400

    Squashed commit of the following:
    commit bd053f46776c086fe504594ccc70be1485da790c
    Author: Charles-MTobias <micael-tobias@hotmail.com>
    Date:   Sun Feb 4 14:54:33 2024 -0400
    
        atualizações
    
    commit 7e6e87d73d22943d361981822a74173b5fadb88c
    Author: Charles-MTobias <micael-tobias@hotmail.com>
    Date:   Sun Feb 4 14:25:27 2024 -0400
    
        anexando o javaScipt

---
+++ script.js
@@ -0,0 +1,57 @@
+/*
+Carne - 400 gr por pessoa   + de 4 horas - 650
+Cerveja - 1200 ml por Pessoa + 4 horas - 2000 ml
+Refrigerante/agua - 1000 ml por pessoa + 4 horas 1500ml
+
+Crianças valem por 0,5
+*/
+
+let inputAdultos = document.getElementById('adultos');
+let inputCriancas = document.getElementById('criancas');
+let inputDuracao = document.getElementById('duracao');
+
+let resultado = document.getElementById('resultado')
+
+function calc(){
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qtdcarne = adultos * canrPP(duracao) + criancas * canrPP(duracao)/2;
+    
+    let qtdcerveja = adultos * cervejaPP(duracao);
+
+    let qtdrefri = adultos * refriPP(duracao) + criancas * refriPP(duracao);
+
+    let qtdApagar = qtdcarne/1000 * 24 + qtdcerveja /250 * 5 + qtdrefri /2000 * 10;
+
+    resultado.innerHTML = `<h3> Quantidades necessárias para o seu churrasco</h3>`
+    resultado.innerHTML += `<p> ${qtdcarne /1000} kg de carne</p>`
+    resultado.innerHTML += `<p> ${Math.ceil(qtdcerveja /250)} Latas de cerveja</p>`
+    resultado.innerHTML += `<p> ${Math.ceil(qtdrefri /2000)} Garrafas de refrigerante e água</p>`
+    resultado.innerHTML += `<p> R$ ${Math.ceil(qtdApagar)},00 Custo extimado do churrasco</p>`
+}
+
+function canrPP(duracao){
+    if (duracao <= 4){
+        return 400;
+    } else {
+        return 650;
+    }
+}
+
+function cervejaPP(duracao){
+    if (duracao <= 4){
+        return 1200;
+    } else {
+        return 2000;
+    }
+}
+
+function refriPP(duracao){
+    if(duracao <= 4){
+        return 1000;
+    } else{
+        return 1500;
+    }
+}
